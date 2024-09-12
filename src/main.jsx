751ba9294e98436eb4ee31bf0f9da916d99f72ee
Edwyn Guzman <edwynjesus30@gmail.com>
2024-09-12 14:42:35 -0400

    restore deploy

--- src/main.jsx
+++ src/main.jsx
@@ -4,15 +4,15 @@ import App from "./App.jsx";
 import "./css/index.css";
 import { ContextProvider } from "./context/Context.jsx";
 import { CssBaseline } from "@mui/material";
-import { BrowserRouter } from "react-router-dom";
+import { HashRouter } from "react-router-dom";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <BrowserRouter>
+    <HashRouter>
       <ContextProvider>
         <CssBaseline />
         <App />
       </ContextProvider>
-    </BrowserRouter>
+    </HashRouter>
   </React.StrictMode>
 );
